8d922df9139cfef795cfa797b4b0c11553fa096e
Casey <casey.olsen@gmail.com>
2021-11-23 11:51:47 -0500

    initialized project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # mednet
+
+To start the app run:
+
+```
+export FLASK_APP=main.py
+set FLASK_APP=main.py
+flask run
+```
---
+++ server.py
@@ -0,0 +1,9 @@
+from flask import Flask
+import os
+
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__, template_folder=tmpl_dir)
+
+@app.route('/')
+def hello():
+    return "Hello World!"
