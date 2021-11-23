bc59123fc1316c227e7f8f9303f3b0ac319c757b
Casey <casey.olsen@gmail.com>
2021-11-23 18:57:48 -0500

    finished most of login and landing pages

--- server.py
+++ server.py
@@ -1,9 +1,33 @@
-from flask import Flask
+from flask import Flask, request, render_template, g, redirect, Response, url_for
+
 import os
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
 
+
 @app.route('/')
-def hello():
-    return "Hello World!"
+def index():
+  return redirect('/login')
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/loginaction', methods=['POST'])
+def login_action():
+    return redirect(url_for('landing'))
+
+@app.route('/createaccount')
+def createaccount():
+    return render_template("create_account.html")
+
+@app.route('/addnewuseraction', methods=['POST'])
+def addnewuser():
+    return redirect(url_for('landing'))
+
+@app.route('/landing')
+def landing():
+    return render_template("landing.html")
+
+
